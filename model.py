7ebaa8ca2582af76a132046bcf1317d6c3cb8a76
Samuel Wu <cernunnos1710@gmail.com>
2024-08-17 14:43:30 +0100

    add VAE model with forward method.

---
+++ model.py
@@ -0,0 +1,35 @@
+from cnn_encoder import CNNEncoder
+from cnn_decoder import CNNDecoder
+import torch
+import torch.nn.functional as F
+from torch import nn, optim
+from utils import *
+
+class VAE(nn.Module):
+    def __init__(self, num_filters, z_dim, learning_rate):
+        """torch nn.Module describing all components to train our VAE."""
+        super(VAE, self).__init__()
+        self.encoder = CNNEncoder(z_dim=z_dim, num_filters=num_filters)
+        self.decoder = CNNDecoder(z_dim=z_dim, num_filters=num_filters)
+        self.learning_rate = learning_rate
+
+    def forward(self, imgs):
+        """Calculates VAE loss for given image batch."""
+        # Encoding and reparameterization
+        mean, log_std = self.encoder(imgs)
+        z = sample_reparameterize(mean, log_std)
+
+        # Decoding
+        decoded_imgs = self.decoder(z)
+
+        # Reconstruction loss
+        imgs = imgs.squeeze(1)  # [B,C,H,W] -> [B,H,W]
+        L_rec = F.cross_entropy(decoded_imgs, imgs, reduction='none').sum([1, 2]).mean()
+
+        # Regularization loss
+        L_reg = KL_divergence(mean, log_std).mean()
+
+        # Bits per dimension
+        bpd = convert_ELBO_to_BPD(L_rec + L_reg, imgs.shape).mean()
+
+        return L_rec, L_reg, bpd
