d4bfc385e8332d9ba9c942ae74ba7316630b5b34
Samuel Wu <cernunnos1710@gmail.com>
2024-08-17 13:44:53 +0100

    add method to convert VAE objective function to BPD score

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 Tensor = torch.Tensor
 
@@ -10,3 +11,7 @@ def KL_divergence(mean, log_std):
     """KL divergence of given distributions to unit Gaussians over last dim."""
     std = torch.exp(log_std)
     return -0.5 * torch.sum(1 + log_std - mean**2 - std**2, dim=-1)
+
+def convert_ELBO_to_BPD(elbo, input_shape):
+    """Converts summed NLL from ELBO to bits per dimension score."""
+    return elbo / (np.prod(input_shape[1:]) * np.log(2))
